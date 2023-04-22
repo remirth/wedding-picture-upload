52385b4a4981e8afac9dc5808c18af147f0b6934
Exsjabe <jakob.bergstrom@explizit.se>
2023-04-22 17:44:05 +0200

    got app dir going

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
-import { type Config } from "tailwindcss";
+import {type Config} from 'tailwindcss';
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['var(--font-jetbrains)'],
+    },
     extend: {},
   },
   plugins: [],
