b307ec50b41a5a4b07b70e7be9d789b8addfd3e2
Exsjabe <jakob.bergstrom@explizit.se>
2023-04-22 22:51:32 +0200

    Got that header links rollin

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,21 +1,53 @@
+'use client';
+
+import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 import {type ChildrenProps} from '~/types/generic';
 
-export const Header = ({}: ChildrenProps) => {
+export const Header = () => {
   return (
     <div className='navbar bg-secondary p-0'>
-      <ul className='grid h-16 w-full grid-cols-3 text-primary'>
-        <ListItem title='Galleri' />
-        <ListItem title='Ladda upp' />
-        <ListItem title='Collage' />
+      <ul className='grid h-16 w-full grid-cols-3'>
+        <ListItem title='Galleri' to='/gallery' />
+        <ListItem title='Ladda upp' to='/upload' />
+        <ListItem title='Collage' to='/collage' />
       </ul>
     </div>
   );
 };
 
-const ListItem = ({title}: {title: string}) => {
+type ListItemProps = {
+  title: string;
+  to: string;
+};
+
+const ListItemClasses = {
+  true: 'bg-secondary-focus border-primary text-primary',
+  false: 'hover:bg-secondary-focus border-secondary-focus',
+} as const satisfies Record<'true' | 'false', string>;
+
+const ListItem = ({title, to}: ListItemProps) => {
+  const isSelected = !!usePathname()?.startsWith(to);
+
   return (
-    <li className='h-full w-full hover:cursor-pointer hover:bg-secondary-focus'>
-      <p className='mx-auto flex h-full w-fit place-items-center'>{title}</p>
+    <li
+      className={`h-full w-full border-b-4 border-solid ${
+        ListItemClasses[String(isSelected) as 'true' | 'false']
+      }`}
+    >
+      <NavLink to={to} isSelected={isSelected}>
+        <p className='mx-auto flex h-full w-fit place-items-center'>{title}</p>
+      </NavLink>
     </li>
   );
+};
+
+const NavLink = ({
+  to,
+  isSelected,
+  children,
+}: {to: string; isSelected: boolean} & ChildrenProps) => {
+  if (isSelected) return <>{children}</>;
+
+  return <Link href={to}>{children}</Link>;
 };
